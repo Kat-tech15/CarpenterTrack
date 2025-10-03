4d41db7214b60637e6581c5c7cde7eb677961505
Kat-tech15 <kelvinkatwai@gmail.com>
2025-10-03 18:29:05 +0300

    applied migrations for orders and products apps

--- orders/views.py
+++ orders/views.py
@@ -1,29 +1,29 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from . models import Order
+from accounts.models import Profile
+from products.models import Product 
 
 
 def place_order(request):
     if request.method == "POST":
-        order_user = request.POST.get('order_user')
-        order_phone_number = request.POST.get('order_phone_number')
-        order_email = request.POST.get('order_email')
-        order_location = request.POST.get('order_location')
         product_name = request.POST.get('product_name')
         quantity = request.POST.get('quantinty')
         deposit = request.POST.get('deposit')
 
+        product = Product.objects.get(name=product_name)
+
+        profile = Profile.objects.get(user=request.user)    
+
         Order.objects.create(
-            order_user=order_user,
-            order_phone_number=order_phone_number,
-            order_email=order_email,
-            order_location=order_location,
-            product_name=product_name,
+            customer=profile,
+            product=product,
             quantity=quantity,
             deposit=deposit
         )
-        return redirect('place_order')
         messages.success(request, "Order placed successfully!")
+        return redirect('place_order')
+    products = Product.objects.all()
     return render(request, 'orders/place_order.html')
 
 def cancel_order(request):
