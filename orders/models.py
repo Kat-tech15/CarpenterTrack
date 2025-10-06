4a3131a1aac737c981e9d860a4771d25ffd7b0cb
Kat-tech15 <kelvinkatwai@gmail.com>
2025-10-06 18:37:57 +0300

    applied migrations

--- orders/models.py
+++ orders/models.py
@@ -6,7 +6,29 @@ class Order(models.Model):
     customer  = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="orders")
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
-    deposit = models.DecimalField(max_digits=8, decimal_places=2)
+    deposit = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    total_price= models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    balance = models.DecimalField(max_digits=8, decimal_places=2, default=0)    
+
+    is_customer_order = models.BooleanField(default=False)
+    custom_description = models.TextField(blank=True, null=True)
+    refrence_image = models.ImageField(upload_to='customer_orders/', blank=True, null=True)
+
+    status = models.CharField(max_length=50, choices=[
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+    ], 
+    default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *rgs, **kwargs):
+        if self.total_price and self.deposit is not None:
+            self.balance = self.total_price - self.deposit
+        super().save(*rgs, **kwargs)
 
     def __str__(self):
-        return f"{self.customer.user.username} - {self.product.name} - {self.quantity} - {self.deposit}"
+        if self.is_customer_order:
+            return f"Custom Order by {self.customer.user.username}"
+        return f"{self.customer.user.username} - {self.product.name if self.product else 'Custom'} - {self.quantity}"
+
+#class payment(models.Model):
--- orders/views.py
+++ orders/views.py
@@ -10,20 +10,41 @@ def place_order(request):
         product_name = request.POST.get('product_name')
         quantity = request.POST.get('quantinty')
         deposit = request.POST.get('deposit')
+        custom_description = request.POST.get('custom_description')
+        refrence_image = request.POST.get('refrence_image')
 
-        product = Product.objects.get(name=product_name)
         profile = Profile.objects.get(user=request.user)    
 
-        Order.objects.create(
-            customer=profile,
-            product=product,
-            quantity=quantity,
-            deposit=deposit
-        )
-        messages.success(request, "Order placed successfully!")
-        return redirect('place_order')
+        if product_name :
+            try: 
+                product = Product.objects.get(name=product_name)
+                order = Order.objects.create(
+                        customer=profile,
+                        product=product,
+                        quantity=quantity,
+                        deposit=deposit,
+                        total_price=product.price * int(quantity),
+                        is_customer_order=False
+                   ) 
+            except Product.DoesNotExist:
+                messages.error(request, "Selected product was not found.")
+                return redirect('place_order')
+        else:
+            order = Order.objects.create(
+                    customer=profile,
+                    quantity=quantity,
+                    deposit=deposit,
+                    total_price=0, 
+                    is_customer_order=True,
+                    custom_description=custom_description,
+                    refrence_image=refrence_image
+                )
+        order.save()
+        messages.success(request, "Order placed successfully.")
+        return redirect('my_orders')
+    
     products = Product.objects.all()
-    return render(request, 'orders/place_order.html', {'products':products})
+    return render(request, 'orders/place_order.html', {'products': products})
 
 def cancel_order(request):
     return render(request, 'orders/cancel_order.html')
