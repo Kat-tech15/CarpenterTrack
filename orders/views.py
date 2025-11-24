599b7a5f98365340ef924b8e47ec066e89c6471c
Katwai <kelvinkatwai@gmail.com>
2025-11-24 17:43:04 +0300

    modified orders/views.py

--- orders/views.py
+++ orders/views.py
@@ -6,56 +6,59 @@ from accounts.models import Profile
 from products.models import Product 
 
 @login_required(login_url='login_view')
-def place_order(request, product_id):
+def place_order(request, product_id=None):
 
-    product = None
-    if product_id:
-        product = get_object_or_404(Product, id=product_id)
+    
+    profile = Profile.objects.get(user=request.user) 
+    products = Product.objects.all()
 
     if request.method == "POST":
+        order_type = request.POST.get('order_type', 'product')
         quantity = int(request.POST.get('quantity',1))
         deposit = float(request.POST.get('deposit',0))
         custom_description = request.POST.get('custom_description', '')
         refrence_image = request.POST.get('refrence_image')
-        selected_peoduct_id = request.POST.get('product_id')
 
-        profile = Profile.objects.get(user=request.user) 
 
-        if posted_product_id and posted_product_id != "":
-            try: 
-                product = Product.objects.get(id=product_id)
-                total_price = float(product.price) * quantity
-
-                order = Order.objects.create(
-                        customer=profile,
-                        product=product,
-                        quantity=quantity,
-                        deposit=deposit,
-                        total_price=total_price,
-                        is_customer_order=False
-                   ) 
-            except Product.DoesNotExist:
-                messages.error(request, "Selected product was not found.")
-                return redirect('place_order', product_id=product_id)
-
-        else:
-            order = Order.objects.create(
-                    customer=profile,
-                    product=None,
-                    quantity=quantity,
-                    deposit=deposit,
-                    total_price=0, 
-                    is_customer_order=True,
-                    custom_description=custom_description,
-                    refrence_image=refrence_image
-                )
+        if order_type == 'product':
+            posted_product_id =  request.POST.get('product_id')
+            if posted_product_id:
+                try:
+                    product = Product.objects.get(id=posted_product_id)        
+                    order = Order.objects.create(
+                            customer=profile,
+                            product=product,
+                            quantity=quantity,
+                            deposit=deposit,
+                            is_customer_order=False,
+                    )
+                    messages.success(request, "Product order placed successfully.")
+                    return redirect('my_orders')
+                except Product.DoesNotExist:
+                    messages.error(request, "Selected product was not found.")
+                    return redirect('place_order')
+            else:
+                messages.error(request, "No product selected.")
             
-        order.save()
-        messages.success(request, "Order placed successfully.")
-        return redirect('my_orders')
+        elif order_type == 'custom':
+            unit_price =  float(request.POST.get('unit_price', 0))
+            total_price = unit_price * quantity
+
+            Order.objects.create(
+                customer=profile,
+                product=None,
+                quantity=quantity,
+                deposit=deposit,
+                total_price=total_price,
+                is_customer_order=True,
+                custom_description=custom_description,
+                refrence_image=refrence_image
+            )
+            messages.success(request, "Custom order placed successfully.")
+            return redirect('my_orders')
     
-    products = Product.objects.all()
     return render(request, 'orders/place_order.html', {'products': products})
+
 
 @login_required(login_url='login_view')
 def cancel_order(request, order_id):
@@ -79,23 +82,36 @@ def edit_order(request, order_id):
     if request.method == "POST":
         new_quantity = int(request.POST.get('quantity', order.quantity))
         new_deposit = float(request.POST.get('deposit', order.deposit))
+        new_description = request.POST.get('custom_desription', order.custom_description)
+        new_image =  request.POST.get('reference_image')
 
         order.quantity = new_quantity
         order.deposit = new_deposit
-        if order.product:
-            order.total_price = order.product.price * new_quantity
+        order.custom_description = new_description
+        
+        if new_image:
+            order.refrence_image = new_image
+
+        if order.is_customer_order:
+            unit_price =  float(request.POST.get('unit_price', 0))
+            order.total_price = unit_price * new_quantity
+        
+        else:
+            order.total_price = float(order.product.price) * new_quantity
         
         order.save()
 
         messages.success(request, "Order updaed successfully!")
         return redirect('my_orders')
+    
+    unit_price = 0
+    if order.is_customer_order and order.quantity:
+        unit_price = float(order.total_price) / order.quantity
 
-
-    return render(request, 'orders/edit_order.html', {'order': order})  
+    return render(request, 'orders/edit_order.html', {'order': order, 'unit_price': unit_price})  
 
 @login_required(login_url='login_view')
 def my_orders(request):
-    
 
     profile = Profile.objects.get(user=request.user)
     active_orders = Order.objects.filter(customer=profile).exclude(status='cancelled') 
